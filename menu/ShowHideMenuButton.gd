e13228cb28dfc5179f9c0805094f9f33fd9d2e12
Max Żurawski <max.zurawski@gmail.com>
2026-03-14 15:50:29 +0100

    Initial commit

---
+++ menu/ShowHideMenuButton.gd
@@ -0,0 +1,19 @@
+extends Button
+
+@export var visibleIcon: Texture2D
+@export var hiddenIcon: Texture2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_on_toggled(button_pressed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_toggled(toggled_on: bool) -> void:
+	print(toggled_on)
+	if toggled_on:
+		icon = visibleIcon
+	elif !toggled_on:
+		icon = hiddenIcon
